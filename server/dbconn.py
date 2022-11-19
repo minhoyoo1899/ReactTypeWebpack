cf954e63925b31e90d808c91f3722282bfeb5e83
MinhoYoo <minhoyoo1899@gmail.com>
2022-11-19 15:05:09 +0900

    update 1119

--- server/dbconn.py
+++ server/dbconn.py
@@ -5,12 +5,12 @@ import pymysql.cursors
 def dbconnect():
     conn = pymysql.connect(
       host='127.0.0.1',
-      port=3307, 
+      port=3306,
       user='root', 
-      password='', 
+      password='TheoHernandez19!',
       db='aitrading_db',
-      charset='utf8', 
-      init_command='SET NAMES UTF8', 
+      charset='utf8',
+      init_command='SET NAMES UTF8',
       cursorclass=pymysql.cursors.DictCursor
       )
     return conn
@@ -19,10 +19,10 @@ def dbconnect():
 def data_by_code(code):
     conn = dbconnect()
     cur = conn.cursor()
-    sql = 'SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_NAME LIKE "%'+code+'_d"'
+    sql = f'SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_NAME LIKE "%{code}_d"'
     cur.execute(sql)
     company = cur.fetchone()
-    sql = 'SELECT * FROM '+company["TABLE_NAME"]+' ORDER BY day DESC'
+    sql = f'SELECT * FROM {company["TABLE_NAME"]} ORDER BY day DESC'
     cur.execute(sql)
     results = cur.fetchmany(10)
     conn.close()
@@ -32,7 +32,7 @@ def data_by_code(code):
 def search_company_name(name):
     conn = dbconnect()
     cur = conn.cursor()
-    sql = f'SELECT name,code FROM `aitrading_db`.`companyList` WHERE name LIKE "%{name}%"'
+    sql = f'SELECT name,code FROM `aitrading_db`.`companyList` WHERE name LIKE "{name}%"'
     cur.execute(sql)
     results = cur.fetchall()
     conn.close()
